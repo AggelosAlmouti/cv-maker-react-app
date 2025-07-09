20683e613f54a5914f4f5c122d6d1fc8890e4963
aggelos <aggelos.almouti@gmail.com>
2025-07-09 13:30:12 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./css/App.css";
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <>
+      <Hello text="hello" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello({ text }) {
+  return <div>{text}</div>;
+}
+
+export default Hello;
